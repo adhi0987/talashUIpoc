8860ec723ee61b4501b257eb6798fbb45c56b7d4
Dasaradha Kalapureddy <dasaradha.kalapureddy@tal.com.au>
2024-10-27 15:50:32 +1100

    upload questions

--- ShareGoodVibe/ClientApp/src/app/contentupload/contentupload.component.ts
+++ ShareGoodVibe/ClientApp/src/app/contentupload/contentupload.component.ts
@@ -14,6 +14,17 @@ export class ContentuploadComponent {
   private baseUrlBlobApi = 'https://talashfileuploadapi-ctapfke2bwcwdghx.australiasoutheast-01.azurewebsites.net/api/blobstorage';
   private baseUrlVideoApi = "https://talashvideo.azurewebsites.net/dyte/v2";
 
+  topicSlected: string = "";
+  files: string[] = [];
+  fileToUpload: FormData | undefined;
+  fileUpload: any;
+  fileUpoadInitiated: boolean = false;
+  fileDownloadInitiated: boolean = false;
+  profile: any;
+  private baseUrl = 'https://talashfileuploadapi-ctapfke2bwcwdghx.australiasoutheast-01.azurewebsites.net/api/blobstorage';
+  public application: string = "";
+  public selectedTopic: string = "";
+
   constructor(private http: HttpClient) {
     //https://localhost:7142/dyte/v2/GetlistOfTopics?userid=test
 
@@ -21,18 +32,96 @@ export class ContentuploadComponent {
       .subscribe((response: any) => {
 
         var json = JSON.parse(JSON.stringify(response));
-       
-        for(var i=0 ; i < json.length ; i++)
-        {
+
+        for (var i = 0; i < json.length; i++) {
           this.topics.push(json[i]);
-        }       
-        
+        }
+
       });
 
   }
 
   CreateNewTopic() {
-    console.log("new topic value: "+this.newTopic);
+    console.log("new topic value: " + this.newTopic);
 
   }
+
+  topicSelected() {
+
+  }
+  upload(files: any) {
+    if (files.length === 0)
+      return;
+
+    const formData = new FormData();
+    let fname = "";
+
+    for (let file of files) {
+      formData.append("asset", file, file.name,);
+      fname = file.name;
+    }
+    this.fileToUpload = formData;
+
+    this.http.post(this.baseUrl + '/insertfile', this.fileToUpload)
+      .subscribe((response: any) => {
+        this.fileUpoadInitiated = false;
+        this.fileUpload = '';
+        if (response == true) {
+          console.log("berofe http get ")
+          //SetFileAttrib?filename=english.txt&userid=dasaradh&application=dyte-videoxxmas
+          let foldername = "";
+
+          this.http.get(this.baseUrl + '/SetFileAttribV2?filename=' + fname + "&userid=" + "Ananymous" + "&topic=" + this.selectedTopic)
+            .subscribe((response: any) => {
+              console.log("uploaded file to directory" + foldername)
+
+            });
+
+        }
+        else {
+          alert('Error occured!');
+          this.fileUpoadInitiated = false;
+        }
+      },
+        err => console.log(err),
+      );
+
+  }
+  handleFileInput(e: any) {
+    let files: any = e.target?.files[0];
+    let formData: FormData = new FormData();
+    formData.append(files[0].name, files[0]);
+    this.fileToUpload = formData;
+    this.onUploadFiles();
+  }
+
+  onUploadFiles() {
+    if (this.fileUpoadInitiated) {
+      return;
+    }
+    this.fileUpoadInitiated = true;
+    if (this.fileToUpload == undefined) {
+      this.fileUpoadInitiated = false;
+      return false;
+    }
+    else {
+      return this.http.post(this.baseUrl + '/insertfile', this.fileToUpload)
+        .subscribe((response: any) => {
+          this.fileUpoadInitiated = false;
+          this.fileUpload = '';
+          if (response == true) {
+            console.log("hrehrehrerherherh");
+            //this.showBlobs();
+          }
+          else {
+            alert('Error occured!');
+            this.fileUpoadInitiated = false;
+          }
+        },
+          err => console.log(err),
+        );
+
+    }
+  }
+
 } 
